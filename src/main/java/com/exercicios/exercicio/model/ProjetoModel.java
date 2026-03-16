88732a531a322d688664d57180e338e6f6cdfc41
gmiashiro <g.miashiro.freitas@gmail.com>
2026-03-16 19:52:45 -0300

    feat: setup inicial do projeto e camada model

---
+++ src/main/java/com/exercicios/exercicio/model/ProjetoModel.java
@@ -0,0 +1,55 @@
+package com.exercicios.exercicio.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_projeto")
+public class ProjetoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "nomeeeee")
+    private String nome;
+    @Column(name = "datadeInicio")
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+
+    public ProjetoModel() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataInicio() {
+        return dataInicio;
+    }
+
+    public void setDataInicio(LocalDate dataInicio) {
+        this.dataInicio = dataInicio;
+    }
+
+    public LocalDate getDataFim() {
+        return dataFim;
+    }
+
+    public void setDataFim(LocalDate dataFim) {
+        this.dataFim = dataFim;
+    }
+}
